8e7648284380c50760d3662095ab0f8f3c5e16d3
Asyrique <asyrique@gmail.com>
2021-05-13 09:21:50 +0800

    change to check for development

--- src/utils.mjs
+++ src/utils.mjs
@@ -1,4 +1,4 @@
 import { google } from "googleapis";
-const credentials = (process.env.NODE_ENV !== "production") ? (await import("../creds/index.mjs")).default : JSON.parse(process.env.SERVICE_ACCOUNT_CREDENTIALS)
+const credentials = (process.env.NODE_ENV === "development") ? (await import("../creds/index.mjs")).default : JSON.parse(process.env.SERVICE_ACCOUNT_CREDENTIALS)
 export const gdrive = google.drive({ version: "v3", auth: await google.auth.getClient({ credentials, scopes: ["https://www.googleapis.com/auth/drive"] })})
 export { DateTime } from "luxon"
