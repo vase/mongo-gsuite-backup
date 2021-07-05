4194dc127b9bb3cf73175f35469bc43bbe0b06e2
Asyrique <asyrique@gmail.com>
2021-07-06 02:17:30 +0800

    move back to utils

--- src/app.js
+++ src/app.js
@@ -2,35 +2,7 @@ import axios from "axios";
 import Bree from "bree";
 import later from "@breejs/later"
 import prettyCron from "prettycron"
-import { google } from "googleapis";
-import { DateTime } from "luxon"
-import { readFile, readdir } from "fs/promises"
-
-// Create base env var object
-const processEnvs = {
-  SHARED_DRIVE_NAME: null,
-  MONGO_HOST_API: null,
-  SERVICE_ACCOUNT_CREDENTIALS: null,
-  TARGET_FOLDER_NAME: null
-}
-
-
-// populate with either docker secrets or envs
-if (process.env.INFRA === "swarm") {
-  for (let env of Object.keys(processEnvs)) {
-    processEnvs[env] = await readFile(`/run/secrets/${env}`, "utf-8")
-  }
-} else if (process.env.INFRA === "fly") {
-  for (let env of Object.keys(processEnvs)) {
-    processEnvs[env] = process.env[env]
-  }
-}
-
-const credentials = JSON.parse(processEnvs.SERVICE_ACCOUNT_CREDENTIALS)
-export const gdrive = google.drive({ version: "v3", auth: await google.auth.getClient({ credentials, scopes: ["https://www.googleapis.com/auth/drive"] })})
-export function mod(n, m) {
-  return ((n % m) + m) % m;
-}
+import { processEnvs, gdrive, DateTime, mod } from "./util.js";
 
 // Get Shared Drive Id
 const driveId = (await gdrive.drives.list()).data.drives.filter(e => e.name === processEnvs.SHARED_DRIVE_NAME).map(e => e.id)[0]
